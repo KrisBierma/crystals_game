f6ed8c9d33a5add12becd5774ac677d7d0d1e372
Kris <kris.bierma@gmail.com>
2018-02-15 20:32:02 -0600

    basic html done, some styling, starting js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+
+//generate random number every time game starts between 19-120
+//generate number for pink, blue, green, and orange crystals between 1-12
+
+//when click on crystal, add ran num of points to total score
+
+
+//when total score equals random number, get a win
+//also reset total score to 0
+//also generate new random number
+//also give 4 crystals new random numbers
+
+
+
+//if total score goes over random number, get a loss
