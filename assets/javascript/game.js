4bf797a4324cc263584dfd4dc00cdc2f83a2c60e
Kris <kris.bierma@gmail.com>
2018-02-16 09:37:40 -0600

    finished js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,70 @@
+    var randomNumber;
+    var blueNum;
+    var pinkNum;
+    var greenNum;
+    var orangeNum;
+    var totalScore;
+    var totalWins=0;
+    var totalLosses=0;
 
+//generate first set of random numbers
+newNumbers();
 
-//generate random number every time game starts between 19-120
+//generate a random number every time game starts between 19-120
 //generate number for pink, blue, green, and orange crystals between 1-12
+function newNumbers(){
+    randomNumber = Math.floor(Math.random()*101)+19;
+    console.log("ranNum="+randomNumber);
+    blueNum = Math.floor(Math.random()*12)+1;
+    pinkNum = Math.floor(Math.random()*12)+1;
+    greenNum = Math.floor(Math.random()*12)+1;
+    orangeNum = Math.floor(Math.random()*12)+1;
+    totalScore=0;
+    $("#randomNum").text(randomNumber);
+}
 
-//when click on crystal, add ran num of points to total score
+//when click on crystal, add random value of points to total score; compare score to random number
+    $("#blue").on("click", function(){
+        totalScore=totalScore+blueNum;
+        $("#score").text(totalScore);
+        console.log("blue="+blueNum);
+        checkScore();
+    })
 
+    $("#pink").on("click", function(){
+        totalScore=totalScore+pinkNum;
+        $("#score").text(totalScore);
+        console.log("pink="+pinkNum);
+        checkScore();
+    })
 
-//when total score equals random number, get a win
-//also reset total score to 0
-//also generate new random number
-//also give 4 crystals new random numbers
+    $("#green").on("click", function(){
+        totalScore=totalScore+greenNum;
+        $("#score").text(totalScore);
+        console.log("green="+greenNum);
+        checkScore();
+    })
 
+    $("#orange").on("click", function(){
+        totalScore=totalScore+orangeNum;
+        $("#score").text(totalScore);
+        console.log("orange"+orangeNum);
+        checkScore();
+    })
 
-
-//if total score goes over random number, get a loss
+//when total score equals random number: get a win, reset total score to 0, generate new random number, give 4 crystals new random numbers
+//when total score is above random number: get a loss, reset total score to 0, generate new numbers
+    function checkScore(){
+        if (totalScore===randomNumber){
+            totalWins++;
+            $("#wins").text(totalWins);
+            $("#score").text(totalScore+ " You won!");
+            newNumbers();
+        }
+        else if (totalScore > randomNumber){
+            totalLosses++;
+            $("#losses").text(totalLosses);
+            $("#score").text(totalScore+ " Too high. That's a loss.");
+            newNumbers();
+        }
+}
